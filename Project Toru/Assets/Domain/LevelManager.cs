ff824555ef80d8a320bf5118b851c7ea1c723c64
Thijs van Rheenen <16120442@student.hhs.nl>
2019-09-26 15:58:22 +0200

    Code cleanup

--- Project Toru/Assets/Domain/LevelManager.cs
+++ Project Toru/Assets/Domain/LevelManager.cs
@@ -15,55 +15,59 @@ public class LevelManager : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		GenerateRooms();
+	}
+
+	// foreach loop that loops through every room and generates the walls and background
+	private void GenerateRooms()
+	{
 		tilemapWalls = walls.GetComponent<Tilemap>();
 		tilemapBackground = background.GetComponent<Tilemap>();
-		GenerateWalls();
-		GenerateBackground();
-	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-
-	void GenerateWalls()
-	{
 		foreach (ScriptableRoom r in rooms)
 		{
-			for (int i = 0; i <= r.width; i++)
-			{
-				tilemapWalls.SetTile(r.position + new Vector3Int(i, 0, 0), r.theme.bottom);
-				tilemapWalls.SetTile(r.position + new Vector3Int(i, r.height, 0), r.theme.top);
-			}
-			for (int j = 0; j <= r.height; j++)
-			{
-				tilemapWalls.SetTile(r.position + new Vector3Int(0, j, 0), r.theme.left);
-				tilemapWalls.SetTile(r.position + new Vector3Int(r.width, j, 0), r.theme.right);
-			}
-			tilemapWalls.SetTile(r.position + new Vector3Int(0, 0, 0), r.theme.bottomLeft);
-			tilemapWalls.SetTile(r.position + new Vector3Int(r.width, 0, 0), r.theme.bottomRight);
-			tilemapWalls.SetTile(r.position + new Vector3Int(0, r.height, 0), r.theme.topLeft);
-			tilemapWalls.SetTile(r.position + new Vector3Int(r.width, r.height, 0), r.theme.topRight);
+			GenerateWalls(r);
+			GenerateBackground(r);
 		}
 	}
 
-	void GenerateBackground()
+	// Generates walls for each room
+	private void GenerateWalls(ScriptableRoom r)
 	{
-		foreach (ScriptableRoom r in rooms)
+		// Sets walls for top and bottom
+		for (int i = 0; i <= r.width; i++)
 		{
-			if (r.lightsOn)
+			tilemapWalls.SetTile(r.position + new Vector3Int(i, 0, 0), r.theme.bottom);
+			tilemapWalls.SetTile(r.position + new Vector3Int(i, r.height, 0), r.theme.top);
+		}
+
+		// Sets walls for both sides
+		for (int j = 0; j <= r.height; j++)
+		{
+			tilemapWalls.SetTile(r.position + new Vector3Int(0, j, 0), r.theme.left);
+			tilemapWalls.SetTile(r.position + new Vector3Int(r.width, j, 0), r.theme.right);
+		}
+
+		// Sets wall corner pieces
+		tilemapWalls.SetTile(r.position + new Vector3Int(0, 0, 0), r.theme.bottomLeft);
+		tilemapWalls.SetTile(r.position + new Vector3Int(r.width, 0, 0), r.theme.bottomRight);
+		tilemapWalls.SetTile(r.position + new Vector3Int(0, r.height, 0), r.theme.topLeft);
+		tilemapWalls.SetTile(r.position + new Vector3Int(r.width, r.height, 0), r.theme.topRight);
+	}
+
+	// fills the background of each room with it's assigned theme tile
+	private void GenerateBackground(ScriptableRoom r)
+	{
+		if (r.lightsOn)
+		{
+			for (int i = 0; i <= r.width; i++)
 			{
-				for (int i = 0; i <= r.width; i++)
+				for (int j = 0; j <= r.height; j++)
 				{
-					for (int j = 0; j <= r.height; j++)
-					{
-						tilemapBackground.SetTile(r.position + new Vector3Int(i, j, 0), r.theme.center);
-					}
+					tilemapBackground.SetTile(r.position + new Vector3Int(i, j, 0), r.theme.center);
 				}
 			}
 		}
 	}
-
 
 }
