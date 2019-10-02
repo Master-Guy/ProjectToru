10f63ecca6d803366b2be95654f59ca2bb4350f7
Thijs van Rheenen <16120442@student.hhs.nl>
2019-10-02 22:24:40 +0200

    First iteration of character movement
    Character movement with WASD and OnMouseClick

---
+++ Project Toru/Assets/Domain/Behaviour/CharacterMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D myRigidbody;
+	private Vector3 change;
+
+	private Vector3 currentLocation;
+	private Vector3 targetLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		change = Vector3.zero;
+		change.x = Input.GetAxisRaw("Horizontal"); // Horizontal input (x)
+		change.y = Input.GetAxisRaw("Vertical"); // Vertical input (y)
+
+		MoveCharacter();
+		MoveCharacterWithMouse();
+	}
+
+	void MoveCharacter()
+	{
+		change.Normalize();
+		myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+	}
+
+	void MoveCharacterWithMouse()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			currentLocation = transform.position;
+			targetLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			targetLocation.z = 1;
+		}
+
+		if (currentLocation != targetLocation)
+		{
+			transform.position = Vector3.Lerp(currentLocation, targetLocation, speed * Time.deltaTime);
+			currentLocation = transform.position;
+		}
+	}
+}
