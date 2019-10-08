d3cceef0dc7cbe1bc091c4b21cb7658739bf6ea6
Erik Bussing <16111842@student.hhs.nl>
2019-10-08 11:38:47 +0200

    initial door

---
+++ Project Toru/Assets/Domain/Behaviour/DoorBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	public RoomBehaviour roomLeft = null, roomRight = null;
+
+	[SerializeField]
+	private Tilemap appearance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
