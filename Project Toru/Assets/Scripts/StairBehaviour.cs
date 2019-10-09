7bb0d2bb1a980f4300f8459721abe4b1bf33b352
Duco Sebel <16050371@student.hhs.nl>
2019-10-09 12:24:33 +0200

    Stairs script

---
+++ Project Toru/Assets/Scripts/StairBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	private Transform player;
+
+	[SerializeField]
+	private Transform target;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("IsColliding");
+
+		player.position = target.position;
+
+		// this was used to diff the player
+		//if (other.CompareTag("Player"))
+		//{
+
+		//}
+	}
+}
