a00bbdbe47ca25b8ad28d046fb493e3e6b09b4f8
Erik Bussing <16111842@student.hhs.nl>
2019-10-10 12:06:23 +0200

    serious Cleanup

---
+++ Project Toru/Assets/Scripts/Behaviour/Character.cs
@@ -0,0 +1,97 @@
+﻿using Assets.Domain.Behaviour;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class SelectedCharacter
+{
+	static Character Selected;
+}
+public class Character : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody2D myRigidbody;
+	private Vector3 change;
+	private Animator animator;
+
+	private List<Item> inventory;
+	private bool didUseStair = false;
+
+	Character()
+	{
+		inventory = new List<Item>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		change = Vector3.zero;
+		change.x = Input.GetAxisRaw("Horizontal");
+		change.y = Input.GetAxisRaw("Vertical");
+
+		// If player releases UP, reset Stair
+		if (didUseStair && change.y > 0)
+		{
+			change.y = 0;
+
+		}
+
+
+		// If player wants to go up, ignore movement
+		else if (didUseStair && change.y <= 0)
+		{
+			didUseStair = false;
+		}
+
+		UpdateAnimationsAndMove();
+	}
+
+	public bool hasKey(int key)
+	{
+		foreach(Item i in inventory)
+		{
+			if (i is Key && ((Key)i).privateKey == key)
+				return true;
+		}
+		return false;
+	}
+
+	public void addItem(Item i)
+	{
+		inventory.Add(i);
+	}
+
+	void UpdateAnimationsAndMove()
+	{
+		if (change != Vector3.zero)
+		{
+			MoveCharacter();
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
+		}
+		else
+		{
+			animator.SetBool("moving", false);
+		}
+	}
+
+	void MoveCharacter()
+	{
+		change.Normalize();
+		myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+	}
+
+	public void StairsTransistion()
+	{
+		didUseStair = true;
+		//Debug.Log("MovedStairs");
+	}
+}
