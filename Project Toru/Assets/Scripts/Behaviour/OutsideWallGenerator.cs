ff939669cabd124dbef2e173663f28d84b28e61c
Duco Sebel <16050371@student.hhs.nl>
2019-10-17 11:49:49 +0200

    Fixing function calls due to new code protection

--- Project Toru/Assets/Scripts/Behaviour/OutsideWallGenerator.cs
+++ Project Toru/Assets/Scripts/Behaviour/OutsideWallGenerator.cs
@@ -17,31 +17,30 @@ public class OutsideWallGenerator : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		////tilemap.SetTile(new Vector3Int(-1, 0, 0), tile);
-		//RoomBehaviour[] rooms = grid.GetComponentsInChildren<RoomBehaviour>();
+		RoomBehaviour[] rooms = building.GetRooms();
 
-		//foreach (RoomBehaviour room in rooms)
-		//{
-		//	// TOP / BOTTOM
-		//	for (int x = -1; x < room.size.x + 1; x++)
-		//	{
-		//		// TOP
-		//		tilemap.SetTile(new Vector3Int(x, room.size.y, 0) + Vector3Int.FloorToInt(room.gameObject.transform.localPosition), tile);
+		foreach (RoomBehaviour room in rooms)
+		{
+			// TOP / BOTTOM
+			for (int x = -1; x < room.GetSize().x + 1; x++)
+			{
+				// TOP
+				tilemap.SetTile(new Vector3Int(x, room.GetSize().y, 0) + room.GetPosition(), tile);
 
-		//		//// BOTTOM
-		//		tilemap.SetTile(new Vector3Int(x, -1, 0) + Vector3Int.FloorToInt(room.gameObject.transform.localPosition), tile);
-		//	}
+				//// BOTTOM
+				tilemap.SetTile(new Vector3Int(x, -1, 0) + room.GetPosition(), tile);
+			}
 
-		//	// LEFT / RIGHT
-		//	for (int y = 0; y < room.size.y; y++)
-		//	{
-		//		// LEFT
-		//		tilemap.SetTile(new Vector3Int(-1, y, 0) + Vector3Int.FloorToInt(room.gameObject.transform.localPosition), tile);
+			// LEFT / RIGHT
+			for (int y = 0; y < room.GetSize().y; y++)
+			{
+				// LEFT
+				tilemap.SetTile(new Vector3Int(-1, y, 0) + room.GetPosition(), tile);
 
-		//		// RIGHT
-		//		tilemap.SetTile(new Vector3Int(room.size.x, y, 0) + Vector3Int.FloorToInt(room.gameObject.transform.localPosition), tile);
-		//	}
-		//}
+				// RIGHT
+				tilemap.SetTile(new Vector3Int(room.GetSize().x, y, 0) + room.GetPosition(), tile);
+			}
+		}
 	}
 
 	// Update is called once per frame
