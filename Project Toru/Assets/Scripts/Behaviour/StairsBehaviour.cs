3c64538466605381106223ffcde83d648b0b0fd9
Duco Sebel <16050371@student.hhs.nl>
2019-10-29 13:59:54 +0100

    Character is using stairs, but wrong target

--- Project Toru/Assets/Scripts/Behaviour/StairsBehaviour.cs
+++ Project Toru/Assets/Scripts/Behaviour/StairsBehaviour.cs
@@ -10,10 +10,10 @@ public class StairsBehaviour : MonoBehaviour
 	public StairsBehaviour Downstairs = null;
 
 	[SerializeField]
-	GameObject GoUpStairs;
+	GameObject GoUpStairs = null;
 
 	[SerializeField]
-	GameObject GoDownStairs;
+	GameObject GoDownStairs = null;
 
 	void Start()
 	{
@@ -28,35 +28,60 @@ public class StairsBehaviour : MonoBehaviour
 		}
 	}
 
-	//public Transform GetUpTarget()
-	//{
-
-	//}
-
-	//public Transform GetDownTarget()
-	//{
-
-	//}
-
-
-	public void UseStairs(bool GoUp, Collider2D character)
+	public Transform GetUpTarget()
 	{
-		Debug.Log("UseStairs");
 
-		//// Move character
-		//other.transform.position = target.position;
+		Debug.Log("1");
+		if (GoUpStairs == null) return null;
+		Debug.Log("2");
+		Transform[] objects = GoUpStairs.GetComponentsInChildren<Transform>();
+		Debug.Log("3");
+		foreach (Transform transformObject in objects)
+		{
+			Debug.Log("4");
+			if (transformObject.tag == "PositionTarget")
+			{
+				Debug.Log("5");
+				return transformObject;
+			}
+		}
+		Debug.Log("6");
+		return null;
+	}
 
-		//// Let character know it is using a stairs
-		//// Get GameObject from collider
-		//GameObject gameobject = other.gameObject;
+	public Transform GetDownTarget()
+	{
 
-		//// Check if this gameobject has an script Character
-		//Character character = (Character)gameobject.GetComponent(typeof(Character));
+		if (GoDownStairs == null) return null;
 
-		//if (character != null)
-		//{
-		//	Debug.Log("Character is using stairs");
-		//	character.StairsTransistion();
-		//}
+		return Downstairs.GetComponent(typeof(Transform)) as Transform;
+	}
+
+
+	public void UseStairs(bool GoUp, Collider2D collider)
+	{
+
+		Transform target = (GoUp ? Upstair.GetUpTarget() : Downstairs.GetDownTarget());
+
+		if (target == null)
+		{
+			Debug.LogError("Stair Target is not set, illegal direction");
+		}
+
+		// Move character
+		collider.transform.position = target.position;
+
+		// Let character know it is using a stairs
+		// Get GameObject from collider
+		GameObject gameobject = collider.gameObject;
+
+		// Check if this gameobject has an script Character
+		Character character = (Character)gameobject.GetComponent(typeof(Character));
+
+		if (character != null)
+		{
+			Debug.Log("Character is using stairs");
+			character.StairsTransistion();
+		}
 	}
 }
