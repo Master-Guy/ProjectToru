d52c7e0590eb2dd35fe7cafcf0d8ad6019aa8a7b
Joost de Niet <17049644@student.hhs.nl>
2019-11-06 15:58:15 +0100

    Switch to pc

---
+++ Project Toru/Assets/Scripts/Behaviour/Camera/CameraItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraItem : MonoBehaviour
+{
+	private static CameraRoom  cameraRoom = new CameraRoom();
+
+	bool isDisabled;
+
+	private void Awake()
+	{
+		isDisabled = false;
+	}
+
+	public void DisableCamera()
+	{
+		isDisabled = true;
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if(!isDisabled && other.isTrigger && other.tag.Equals("Player"))
+		{
+			Debug.Log("You have been seen");
+			cameraRoom.AlertGuard();
+		}
+	}
+}
---
+++ Project Toru/Assets/Scripts/Behaviour/Camera/CameraRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRoom : MonoBehaviour
+{
+	public GameObject room;
+	Room roomObj;
+
+	GameObject[] cameras;
+
+	void Awake()
+	{
+		roomObj = room.GetComponent<Room>();
+		cameras = GameObject.FindGameObjectsWithTag("Camera");
+	}
+
+	public void AlertGuard()
+	{		
+		/*if(roomObj.getNPCsInRoom().Count > 0)
+		{
+			foreach(GameObject npc in roomObj.getNPCsInRoom())
+			{
+				npc.GetComponent<NPC>().Say("I am warned!");
+			}
+			Debug.Log("");
+		}*/
+	}
+}
