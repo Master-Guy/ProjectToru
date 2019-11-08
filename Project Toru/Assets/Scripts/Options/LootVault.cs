cf7a4eebd119dd61781c8dd614dc9117ec74c2bf
Erik Bussing <16111842@student.hhs.nl>
2019-11-08 11:30:08 +0100

    Auto stash before cherrypick of "Options or actors can now be removed from an option if necessary"

---
+++ Project Toru/Assets/Scripts/Options/LootVault.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Options
+{
+	public class LootVault : Option
+	{
+		public override void Activate()
+		{
+
+		}
+	}
+}
