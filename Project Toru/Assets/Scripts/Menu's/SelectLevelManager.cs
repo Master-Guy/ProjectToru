3bd6074662ea70bd477a9b8d38e01ff6dfdcf3e4
Erik <12006629@student.hhs.nl>
2019-11-26 15:38:31 +0100

    Level Select start
    Level selection from the main menu, new scenes need to be added aswell as the connection and naming of scenes for levels.

---
+++ Project Toru/Assets/Scripts/Menu's/SelectLevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectLevelManager : MonoBehaviour
+{
+    public Button mainMenuButton;
+    public Button LevelOneButton;
+    public Button LevelTwoButton;
+    public Button LevelThreeButton;
+
+    private void Start()
+    {
+        mainMenuButton.onClick.AddListener(delegate { OnMainMenuClick(); });
+        LevelOneButton.onClick.AddListener(delegate { OnLevelOneClick(); });
+        LevelTwoButton.onClick.AddListener(delegate { OnLevelTwoClick(); });
+        LevelThreeButton.onClick.AddListener(delegate { OnLevelThreeClick(); });
+    }
+
+    public void OnLevelOneClick()
+    {
+        SceneManager.LoadScene("NewStairs");
+    }
+
+    public void OnLevelTwoClick()
+    {
+        SceneManager.LoadScene("NewStairs");
+    }
+
+    public void OnLevelThreeClick()
+    {
+        SceneManager.LoadScene("NewStairs");
+    }
+
+    public void OnMainMenuClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
