d0cca2b1e4960e82aea24cb49b442d2ae8831718
Damian Meere <17073413@student.hhs.nl>
2019-11-28 11:59:30 +0100

    character + UI

---
+++ Project Toru/Assets/Scripts/Behaviour/CharacterSlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSlot : MonoBehaviour
+{
+    public Sprite icon;
+    Character character;
+
+    public void AddCharacter(Character newCharacter)
+    {
+        character = newCharacter;
+    }
+
+    public void setSprite(Sprite s)
+    {
+        this.icon = s;
+        transform.GetComponent<Image>().sprite = this.icon;
+        transform.GetComponent<Mask>().showMaskGraphic = true;
+        transform.GetChild(0).GetComponent<Button>().interactable = true;
+
+    }
+
+    public void SelectCharacter()
+    { 
+        Character.selectedCharacter = character.gameObject;
+        Debug.Log("Character Selected!" + Character.selectedCharacter);
+    }
+
+}
---
+++ Project Toru/Assets/Scripts/Behaviour/CharacterUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUI : MonoBehaviour
+{
+    List<GameObject> players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.GetChild(0).name);
+        int loop = 0;
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            Sprite s = obj.GetComponent<SpriteRenderer>().sprite;
+            transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().AddCharacter(obj.GetComponent<Character>());
+            transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().setSprite(s);
+            transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().SelectCharacter();
+            loop++;
+        }
+
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        
+    }
+}
