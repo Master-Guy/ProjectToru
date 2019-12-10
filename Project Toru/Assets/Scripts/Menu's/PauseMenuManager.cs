cc6a642e0e0b4e5edefe38c441fdfcce9fcad79c
Erik Engelgeer <12006629@student.hhs.nl>
2019-12-10 15:20:29 +0100

    Start of the pause menu

---
+++ Project Toru/Assets/Scripts/Menu's/PauseMenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuPrefab;
+    private Canvas _c;
+
+    private Canvas SceneCanvas
+    {
+        get
+        {
+            if (_c == null)
+            {
+                _c = FindObjectOfType<Canvas>();
+
+                if (_c == null)
+                {
+                    _c = Instantiate(new Canvas());
+                }
+            }
+
+            return _c;
+        }
+    }
+
+    private GameObject pauseMenuInstance;
+
+   public void OpenMenu()
+    {
+        if (pauseMenuInstance == null)
+        {
+            Instantiate(pauseMenuPrefab, SceneCanvas.transform);
+        } else
+        {
+            pauseMenuInstance.SetActive(true);
+        }
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+            OpenMenu();
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+}
