9078aa975f4e169e18d41fe8a5203e1f23ca37ad
Duco Sebel <16050371@student.hhs.nl>
2020-01-10 12:12:51 +0100

    Disable BackgroundAnimator instead of constantly checking if it should be used when no camera is attachted

--- Project Toru/Assets/Art/Background/BackgroundAnimator.cs
+++ Project Toru/Assets/Art/Background/BackgroundAnimator.cs
@@ -13,13 +13,19 @@ public class BackgroundAnimator : MonoBehaviour
     [SerializeField]
     GameObject SkyscraperBack = null;
 
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            this.enabled = false;
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (mainCamera != null)
-        {
-            SkyscraperFront.transform.position = new Vector3(mainCamera.gameObject.transform.position.x * 0.2f, SkyscraperFront.transform.position.y, SkyscraperFront.transform.position.z);
-            SkyscraperBack.transform.position = new Vector3(mainCamera.gameObject.transform.position.x * 0.1f, SkyscraperBack.transform.position.y, SkyscraperBack.transform.position.z);
-        }
+        SkyscraperFront.transform.position = new Vector3(mainCamera.gameObject.transform.position.x * 0.2f, SkyscraperFront.transform.position.y, SkyscraperFront.transform.position.z);
+        SkyscraperBack.transform.position = new Vector3(mainCamera.gameObject.transform.position.x * 0.1f, SkyscraperBack.transform.position.y, SkyscraperBack.transform.position.z);
     }
 }
