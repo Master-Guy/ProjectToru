f9320791351a1313538e61f15f5659a88082d723
Duco Sebel <16050371@student.hhs.nl>
2020-01-12 15:42:00 +0100

    Adding GameProgression event system

---
+++ Project Toru/Assets/Level1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1 : LevelScript
+{
+	[SerializeField]    
+	Vault vault = null;
+	
+	[SerializeField]   
+	Door vaultRoomDoor = null;
+	
+	void Start() {
+		vault.levelScript = this;
+		
+		on("vault_open", () => {
+			vaultRoomDoor.Close();
+		});
+	}
+}
---
+++ Project Toru/Assets/LevelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void LevelScriptCallback();
+
+public class LevelScript : MonoBehaviour
+{
+	
+	static Dictionary<string, LevelScriptCallback> events = new Dictionary<string, LevelScriptCallback>();
+	
+	public void emit(string eventString)
+	{
+		Debug.Log("Emitting event " + eventString);
+		events[eventString]?.Invoke();
+	}
+	
+	public void on(string eventString, LevelScriptCallback callback)
+	{
+		
+		events.Add(eventString, callback);
+		
+	}
+}
