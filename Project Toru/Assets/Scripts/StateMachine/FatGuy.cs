5c7ab3771b270535767caa74b52e7688a1ad2ba5
Duco Sebel <16050371@student.hhs.nl>
2020-01-30 14:56:30 +0100

    Fixing reference errors

---
+++ Project Toru/Assets/Scripts/StateMachine/FatGuy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatGuy : NPC
+{
+    private bool surrender = false;
+    private bool fleeTrue = false;
+
+    public void Start()
+    {
+        this.startingPosition = transform.position;
+        this.animator = GetComponent<Animator>();
+        PingPong();
+    }
+
+    public void Update()
+    {
+        this.statemachine.ExecuteStateUpdate();
+        AdjustOrderLayer();
+        FleeIfPossible();
+        showCountdown();
+    }
+
+    void OnMouseDown()
+    {
+        Surrender();
+    }
+
+    void Surrender()
+    {
+        if (currentRoom.SelectedPlayerInRoom() && !surrender)
+        {
+            this.surrender = true;
+            this.statemachine.ChangeState(new Surrender(this.animator));
+            Say("Don't shoot!");
+            dropBag();
+        }
+    }
+
+    void FleeIfPossible()
+    {
+        if (currentRoom == null) return;
+
+        if (!currentRoom.AnyCharacterInRoom() && surrender)
+        {
+            this.surrender = false;
+            this.statemachine.ChangeState(new Idle(this.animator));
+			gameObject.GetComponent<ExecutePathFindingNPC>().setPosTarget(-30, 1);
+            fleeTrue = true;
+        }
+    }
+
+    void showCountdown()
+    {
+        if (fleeTrue)
+        {
+            Invoke("startCountDown", 8);
+            fleeTrue = false;
+        }
+    }
+
+    void startCountDown()
+    {
+		LevelManager.emit("EmployeeFleed");
+    }
+}
