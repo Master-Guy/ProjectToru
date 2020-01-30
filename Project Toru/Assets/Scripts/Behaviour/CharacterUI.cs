3d6847cd7aa9953c32f19fb375c0226885d4318f
Damian Meere <17073413@student.hhs.nl>
2020-01-30 11:27:33 +0100

    onePlayer no selection

--- Project Toru/Assets/Scripts/Behaviour/CharacterUI.cs
+++ Project Toru/Assets/Scripts/Behaviour/CharacterUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterUI : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject CharacterSelectionBox;
 
     void Start()
     {
@@ -12,15 +13,21 @@ public class CharacterUI : MonoBehaviour
 
 		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-		foreach (GameObject obj in players)
+        if(players.Length == 1)
         {
-            GameObject spawn = Instantiate(prefab);
-            spawn.transform.SetParent(transform.GetChild(0));
-           
-            Sprite s = obj.GetComponent<SpriteRenderer>().sprite;
-            transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().AddCharacter(obj.GetComponent<Character>());
-            transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().setSprite(s);
-            loop++;
+            CharacterSelectionBox.SetActive(false);
+        } else
+        {
+            foreach (GameObject obj in players)
+            {
+                GameObject spawn = Instantiate(prefab);
+                spawn.transform.SetParent(transform.GetChild(0));
+
+                Sprite s = obj.GetComponent<SpriteRenderer>().sprite;
+                transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().AddCharacter(obj.GetComponent<Character>());
+                transform.GetChild(0).GetChild(loop).GetComponent<CharacterSlot>().setSprite(s);
+                loop++;
+            }
         }
 
 		transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(100 , players.Length * 100);
